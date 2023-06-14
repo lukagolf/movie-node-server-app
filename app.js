06b8a5bcdfe4d7e3f9d9e045b319365d2756229a
lukagolf <jovanovic.l@northeastern.edu>
2023-06-14 12:57:02 -0400

    Working login and register

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ import UserController from "./users/users-controller.js";
 import ReviewsController from "./controllers/reviews/reviews-controller.js";
 import session from "express-session";
 import AuthController from "./users/auth-controller.js";
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/movie';
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(
