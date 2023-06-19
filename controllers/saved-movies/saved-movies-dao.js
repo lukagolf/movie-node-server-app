34f75f26c9b79f406727c330f0adc09ca45792d5
kzeng24 <kathyzeng2002@gmail.com>
2023-06-19 02:11:28 -0400

    fixed controller for saved movies such that it takes in a user id

--- controllers/saved-movies/saved-movies-dao.js
+++ controllers/saved-movies/saved-movies-dao.js
@@ -1,5 +1,5 @@
 import savedMoviesModel from "./saved-movies-model.js";
 
-export const findSavedMovies = () => savedMoviesModel.find();
+export const findSavedMovies = (uid) => savedMoviesModel.find({userId: uid});
 export const createSavedMovie = (movie) => savedMoviesModel.create(movie);
 export const deleteSavedMovie = (mid) => savedMoviesModel.deleteOne({id: mid});
